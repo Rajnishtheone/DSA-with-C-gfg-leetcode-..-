521b4c36f7e33a341b8cbcf9c38c8061f83a6513
Rajnish Kumar <116073066+Rajnishtheone@users.noreply.github.com>
2025-03-03 20:39:18 +0530

    Time: 9 ms (40.37%) | Memory: 140.9 MB (13.93%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.cpp
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.cpp
@@ -2,24 +2,20 @@ class Solution {
 public:
     vector<int> pivotArray(vector<int>& nums, int pivot) {
         vector<int> less, equal, greater;
-        
-        // Separate elements into three groups
+
+        // Partition the elements into three separate vectors
         for (int num : nums) {
-            if (num < pivot) {
-                less.push_back(num);
-            } else if (num == pivot) {
-                equal.push_back(num);
-            } else {
-                greater.push_back(num);
-            }
+            if (num < pivot) less.push_back(num);
+            else if (num == pivot) equal.push_back(num);
+            else greater.push_back(num);
         }
-        
-        // Concatenate the groups in the correct order
+
+        // Combine the three vectors
         vector<int> ans;
         ans.insert(ans.end(), less.begin(), less.end());
         ans.insert(ans.end(), equal.begin(), equal.end());
         ans.insert(ans.end(), greater.begin(), greater.end());
-        
+
         return ans;
     }
 };
