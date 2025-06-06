9e98cf9a1ca7691110e00891d8424ec1b23b3865
Rajnish Kumar <116073066+Rajnishtheone@users.noreply.github.com>
2025-06-06 23:34:59 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (24.91%) - LeetSync

--- 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
@@ -1,29 +1,23 @@
 class Solution {
 public:
     string removeOuterParentheses(string s) {
-        string result;   // To store the final result
-        int balance = 0; // To keep track of the balance of parentheses
-
-        // Iterate through each character in the string
-        for (int i = 0; i < s.size(); i++) {
-            if (s[i] == '(') {
-                // If balance is greater than 0, it means this '(' is not an
-                // outermost parenthesis
-                if (balance > 0) {
-                    result += s[i]; // Add the character to the result
+        string result;
+        int depth=0;
+        for(int i=0;i<s.length();i++){
+            if(s[i]=='('){
+                if(depth>0){
+                    result += s[i];
                 }
-                balance++; // Increase the balance for '('
-            } else {
-                balance--; // Decrease the balance for ')'
-                // If balance is greater than 0, it means this ')' is not an
-                // outermost parenthesis
-                if (balance > 0) {
-                    result += s[i]; // Add the character to the result
+                depth++;
+            }
+            else{
+                depth--;
+                if(depth>0){
+                    result+=s[i];
                 }
             }
         }
-
         return result;
-        
+
     }
 };
