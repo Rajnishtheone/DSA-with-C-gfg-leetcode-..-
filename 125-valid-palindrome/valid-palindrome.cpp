6ec8a487527745b7151798e80bc0b57b46cd541c
Rajnish Kumar <116073066+Rajnishtheone@users.noreply.github.com>
2025-08-05 22:54:54 +0530

    Time: 4 ms (16.99%) | Memory: 9.7 MB (94.21%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int start =0;
+        int n=s.length();
+        int end = n-1;
+
+        while(start<end){
+            if(!isalnum(s[start]))
+            {start++;
+            continue;
+            }
+            if(!isalnum(s[end])){
+            
+            end--;
+            continue;
+
+            }
+
+            if(tolower(s[start]) != tolower(s[end])){
+                return false;
+            }
+
+            start++;
+            end--;
+        }
+
+        return true;
+
+    }
+};
