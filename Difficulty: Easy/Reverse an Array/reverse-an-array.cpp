21af13c5228c8d167225d3c0f3a1ceb77c9e8c2d
Rajnish Kumar <116073066+Rajnishtheone@users.noreply.github.com>
2025-08-09 18:56:02 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Reverse an Array/reverse-an-array.cpp
+++ Difficulty: Easy/Reverse an Array/reverse-an-array.cpp
@@ -1,43 +1,15 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
 class Solution {
   public:
     void reverseArray(vector<int> &arr) {
         // code here
-         int n = arr.size();
-        for (int i = 0; i < n / 2; i++) {
-            swap(arr[i], arr[n - i - 1]); // Swap elements from both ends
+        int n = arr.size();
+        int start = 0;
+        int end = n-1;
+        
+        while(start < end){
+            swap(arr[start] , arr[end]);
+            start++;
+            end--;
         }
     }
 };
-
-//{ Driver Code Starts.
-int main() {
-    int t;
-    cin >> t;
-    cin.ignore();
-    while (t--) {
-        vector<int> arr;
-        string input;
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            arr.push_back(number);
-        }
-
-        Solution ob;
-        ob.reverseArray(arr);
-        for (int i = 0; i < arr.size(); i++) {
-            cout << arr[i] << " ";
-        }
-        cout << endl;
-        cout << "~" << endl;
-    }
-    return 0;
-}
-// } Driver Code Ends
