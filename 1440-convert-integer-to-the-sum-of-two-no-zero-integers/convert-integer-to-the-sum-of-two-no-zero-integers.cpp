6a6f28302dc867e5984f526b1c89262500617dd3
Rajnish Kumar <116073066+Rajnishtheone@users.noreply.github.com>
2025-09-08 23:33:38 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (85.27%) - LeetSync

--- 1440-convert-integer-to-the-sum-of-two-no-zero-integers/convert-integer-to-the-sum-of-two-no-zero-integers.cpp
+++ 1440-convert-integer-to-the-sum-of-two-no-zero-integers/convert-integer-to-the-sum-of-two-no-zero-integers.cpp
@@ -1,33 +1,22 @@
 class Solution {
 public:
+    bool iszero(int temp) {
+       
+        while (temp > 0) {
+            if (temp % 10 == 0)
+                return false;
+            temp = temp / 10;
+        }
+        return true;
+    }
     vector<int> getNoZeroIntegers(int n) {
         vector<int> res;
-        for (int i = 1; i <= n; i++) {
-            int temp = i;
-            bool r = false;
-            while (temp > 0) {
-                if (temp % 10 == 0)
-                    r = true;
-                temp = temp / 10;
-            }
-            if (r)
-                continue;
-            for (int j = 1; j <= n; j++) {
-                int temp2 = j;
-                bool rs = false;
-                while (temp2 > 0) {
-                    if (temp2 % 10 == 0)
-                        rs=true;
-                    temp2 = temp2 / 10;
-                }
-                if (rs)
-                    continue;
-
-                if (i + j == n) {
-                    res.push_back(i);
-                    res.push_back(j);
-                    return res;
-                }
+        int j;
+        for (int i = 1; i < n; i++) {
+            j = n - i;
+            if (iszero(i) && iszero(j)) {
+                res = {i, j};
+                break;
             }
         }
         return res;
