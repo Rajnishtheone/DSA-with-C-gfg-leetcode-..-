f705c1e9d4e169c35da3dfe82a12d4a0fe7236f1
Rajnish Kumar <116073066+Rajnishtheone@users.noreply.github.com>
2025-10-01 23:09:01 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (80.28%) - LeetSync

---
+++ 1642-water-bottles/water-bottles.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int numWaterBottles(int numBottles, int numExchange) {
+        int total = numBottles;
+        int empty = numBottles;
+
+        while (empty >= numExchange) {
+            int newBottle = empty / numExchange;
+            total = total + newBottle;
+            empty = empty % numExchange + newBottle;
+        }
+
+        return total;
+    }
+};
