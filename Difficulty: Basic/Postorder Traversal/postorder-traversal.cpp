0073fbbbb2a1d3baa810c36e2205a5e1b049df1e
Rajnish Kumar <116073066+Rajnishtheone@users.noreply.github.com>
2025-10-05 20:51:42 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Postorder Traversal/postorder-traversal.cpp
@@ -0,0 +1,43 @@
+/*
+class Node {
+  public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+*/
+
+class Solution {
+  public:
+    vector<int> postOrder(Node* root) {
+        stack<Node*>s;
+        vector<int>ans;
+        if(root == NULL)return ans;
+        s.push(root);
+        
+        while(!s.empty()){
+            Node* temp = s.top();
+            s.pop();
+            ans.push_back(temp->data);
+            
+            if(temp->left){
+                s.push(temp->left);
+            }
+            
+            if(temp->right){
+                s.push(temp->right);
+            }
+        }
+        
+        reverse(ans.begin(),ans.end());
+        
+        return ans;
+        
+    }
+};
