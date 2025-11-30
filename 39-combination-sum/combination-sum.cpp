1d8352f663ed8bd1068b9c421101e91c5b3f4b82
Rajnish Kumar <116073066+Rajnishtheone@users.noreply.github.com>
2025-11-30 20:59:35 +0530

    Time: 7 ms (28.25%) | Memory: 19.2 MB (20.54%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,25 +1,41 @@
 class Solution {
 public:
-    void backtrack(int start , vector<int>&candidate,int target,int sum,vector<vector<int>>&result,vector<int>path){
-        if(sum == target){
-            result.push_back(path);
+    set<vector<int>> s;
+    void getallcombination(vector<int>& candidates, int indx, int target,
+                           vector<vector<int>>& ans, vector<int>& comb) {
+        if (indx == candidates.size() || target < 0)
             return;
+
+        if (target == 0) {
+            if (s.find(comb) == s.end()) {
+                ans.push_back(comb);
+                s.insert(comb);
+            }
         }
-        if(sum > target){
-            return;
-        }
-        for(int i=start;i<candidate.size();i++){
-            path.push_back(candidate[i]);
-            backtrack(i,candidate,target,sum+candidate[i],result,path);
-            path.pop_back();
-        }
+
+        comb.push_back(candidates[indx]);
+
+        // single
+
+        getallcombination(candidates, indx + 1, target - candidates[indx], ans,
+                          comb);
+
+        // multiple
+
+        getallcombination(candidates, indx, target - candidates[indx], ans,
+                          comb);
+
+        // not included
+        comb.pop_back();
+        getallcombination(candidates, indx + 1, target, ans, comb);
     }
 
-
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<vector<int>>result;
-        vector<int>path;
-        backtrack(0,candidates,target,0,result,path);
-        return result;
+        vector<int> comb;
+        vector<vector<int>> ans;
+
+        getallcombination(candidates, 0, target, ans, comb);
+
+        return ans;
     }
 };
